363a600217651922fe366cf88fae2a7074c880e5
Ugo Briasco <ugo.briasco@sumup.com>
2021-10-14 16:03:23 +0200

    Structure npm package

--- index.js
+++ index.js
@@ -1,14 +1,3 @@
-const Worker = require("./Worker");
+const { Worker, EventsBroker } = require("./lib");
 
-const myEventEmitter = new Worker();
-myEventEmitter.loop(6000, () => commonEvent.emit("myEvent", "Hello there"));
-
-const myWorker = new Worker();
-
-// Loopable events
-const build = () => console.log("Build");
-const sleep = () => console.log("Sleep");
-myWorker.loop(1000, () => build());
-myWorker.loop(2000, () => sleep());
-
-myWorker.listen("myEvent", data => console.log(data));
+module.exports = { Worker, EventsBroker };
